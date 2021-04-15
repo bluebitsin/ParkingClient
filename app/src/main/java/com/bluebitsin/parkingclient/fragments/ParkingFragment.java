17957b4529801a4691627765e4df172f0b00241f
abhishek <abhishek@gmail.com>
2021-04-15 21:02:21 +0530

    Parking Lot Adapter Added

--- app/src/main/java/com/bluebitsin/parkingclient/fragments/ParkingFragment.java
+++ app/src/main/java/com/bluebitsin/parkingclient/fragments/ParkingFragment.java
@@ -1,12 +1,63 @@
 package com.bluebitsin.parkingclient.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bluebitsin.parkingclient.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 
-public class ParkingFragment extends Fragment {
+public class ParkingFragment extends Fragment implements OnMapReadyCallback{
 
     public ParkingFragment(){
         super(R.layout.fragment_parking);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_parking, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        return view;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        LatLng sydney = new LatLng(-33.88,151.21);
+        LatLng mountainView = new LatLng(37.4, -122.1);
+
+// Move the camera instantly to Sydney with a zoom of 15.
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+// Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomIn());
+
+// Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+// Construct a CameraPosition focusing on Mountain View and animate the camera to that position.
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(mountainView )      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
---
+++ app/src/main/java/com/bluebitsin/parkingclient/model/ParkingLot.java
@@ -0,0 +1,85 @@
+package com.bluebitsin.parkingclient.model;
+
+import java.io.Serializable;
+
+public class ParkingLot implements Serializable {
+
+    private int parkingLotId;
+    private String name;
+    private String address;
+    private int totalBlocks;
+    private int zipcode;
+    private String operatingCompany;
+
+    public ParkingLot(int parkingLotId, String name, String address, int totalBlocks, int zipcode, String operatingCompany) {
+        this.parkingLotId = parkingLotId;
+        this.name = name;
+        this.address = address;
+        this.totalBlocks = totalBlocks;
+        this.zipcode = zipcode;
+        this.operatingCompany = operatingCompany;
+    }
+
+    public ParkingLot() {
+    }
+
+    public int getParkingLotId() {
+        return parkingLotId;
+    }
+
+    public void setParkingLotId(int parkingLotId) {
+        this.parkingLotId = parkingLotId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getTotalBlocks() {
+        return totalBlocks;
+    }
+
+    public void setTotalBlocks(int totalBlocks) {
+        this.totalBlocks = totalBlocks;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getOperatingCompany() {
+        return operatingCompany;
+    }
+
+    public void setOperatingCompany(String operatingCompany) {
+        this.operatingCompany = operatingCompany;
+    }
+
+    @Override
+    public String toString() {
+        return "ParkingLot{" +
+                "parkingLotId=" + parkingLotId +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", totalBlocks=" + totalBlocks +
+                ", zipcode=" + zipcode +
+                ", operatingCompany='" + operatingCompany + '\'' +
+                '}';
+    }
+}
